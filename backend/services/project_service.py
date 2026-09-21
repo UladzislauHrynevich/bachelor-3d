075d3deab5045564d4efee18982984855a308f35
UladzislauHrynevich <vladisgrinevich@gmail.com>
2026-09-21 14:14:34 +0200

    add func create_project

--- backend/services/project_service.py
+++ backend/services/project_service.py
@@ -1,1 +1,12 @@
-#ki
+from sqlalchemy.orm import Session
+
+from backend.database.models import Project
+
+def create_project(db: Session):
+    project = Project()
+    db.add(project)
+    db.commit()
+    db.refresh(project)
+
+
+
