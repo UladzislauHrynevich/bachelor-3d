318595da7ab8241d1f6374a884032b9f9c9b24b2
UladzislauHrynevich <vladisgrinevich@gmail.com>
2026-09-23 15:07:43 +0200

    add class ProjectImage

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlalchemy import DateTime, String, func
+from sqlalchemy import DateTime, String, func, ForeignKey, BigInteger
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.database.database import Base
 
@@ -30,4 +30,40 @@ class Project(Base):
         server_default=func.now(),
         onupdate=func.now()
     )
-    
+    images:Mapped[list["ProjectImage"]] = relationship(
+        back_populates="project"
+    )
+
+class ProjectImage(Base):
+    __tablename__ = "images"
+    id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4
+    )
+    project_id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        ForeignKey("projects.id"),
+        nullable=False
+    )
+    original_name: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+    storage_path:Mapped[str] = mapped_column(
+        String(),
+        nullable=False
+    )
+    size_bytes:Mapped[int] = mapped_column(
+        BigInteger,
+        nullable=False
+
+    )
+    created_at:Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
+    project: Mapped["Project"] = relationship(
+    back_populates="images"
+)
